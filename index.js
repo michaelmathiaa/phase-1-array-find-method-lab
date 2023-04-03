8236ac555e38a0f86e05f3d6642efca2a7146f0c
Michael Mathias <michaelscodingjourney@gmail.com>
2023-04-03 11:04:02 -0700

    Did not originally use find()

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 // code your solution here 
 
 const superbowlWin = array => {
-    for(const win of array){
-        if(win.result === 'W'){
-            return win.year;
-        }
-    }
+    let winResult = array.find(cbArray => cbArray.result === 'W');
+    if(winResult){
+        return winResult.year;
+    } 
 }
